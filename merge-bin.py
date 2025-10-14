7e7bc9d0adbd74d2771816467bf3b27831d53c7d
CDFER <cdirksfer@gmail.com>
2025-10-14 14:03:03 +1300

    Fix path bugs in merge-bin.py

--- merge-bin.py
+++ merge-bin.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python3
-
-# Adds PlatformIO post-processing to merge all the ESP flash images into a single image.
+#!/usr/bin/env python3
 
 import os
 
 Import("env", "projenv")
 
 board_config = env.BoardConfig()
-firmware_bin = "${BUILD_DIR}/${PROGNAME}.bin"
-merged_bin = os.environ.get("MERGED_BIN_PATH", "${BUILD_DIR}/${PROGNAME}-merged.bin")
+
+firmware_bin = os.path.join("${BUILD_DIR}", "${PROGNAME}.bin")
+default_merged_bin = os.path.join("${BUILD_DIR}", "${PROGNAME}-merged.bin")
+merged_bin = os.environ.get("MERGED_BIN_PATH", default_merged_bin)
 
 
 def merge_bin_action(source, target, env):
@@ -17,25 +17,34 @@ def merge_bin_action(source, target, env):
         "$ESP32_APP_OFFSET",
         source[0].get_abspath(),
     ]
-    merge_cmd = " ".join(
-        [
-            '"$PYTHONEXE"',
-            '"$OBJCOPY"',
-            "--chip",
-            board_config.get("build.mcu", "esp32"),
-            "merge_bin",
-            "-o",
-            merged_bin,
-            "--flash_mode",
-            "keep",
-            "--flash_freq",
-            "${__get_board_f_flash(__env__)}",
-            "--flash_size",
-            board_config.get("upload.flash_size", "4MB"),
-            *flash_images,
-        ]
+
+    merge_cmd = [
+        "$PYTHONEXE",
+        "$OBJCOPY",
+        "--chip",
+        board_config.get("build.mcu", "esp32"),
+        "merge_bin",
+        "-o",
+        merged_bin,
+        "--flash_mode",
+        "keep",
+        "--flash_freq",
+        "${__get_board_f_flash(__env__)}",
+        "--flash_size",
+        board_config.get("upload.flash_size", "4MB"),
+    ]
+
+    # Add flash images (address + file pairs)
+    for item in flash_images:
+        merge_cmd.append(
+            env.subst(item)
+        )  # Ensure variables like $ESP32_APP_OFFSET are expanded
+
+    env.Execute(
+        env.VerboseAction(
+            " ".join(['"' + arg + '"' for arg in merge_cmd]), "Merging firmware..."
+        )
     )
-    env.Execute(merge_cmd)
 
 
 env.AddCustomTarget(
